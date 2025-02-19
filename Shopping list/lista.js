18ad11c0be11eb250cdbc42d35ab8206cc9b7310
Vincenza <vinni2690@hotmail.com>
2025-02-19 16:47:33 +0100

    JS almost complete

--- Shopping list/lista.js
+++ Shopping list/lista.js
@@ -6,9 +6,60 @@ pageForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const addItemInput = document.getElementById('add-item')
+
+    const newItemValue = {
+        newItem: addItemInput.value
+    }
+
+    // create new element
+    const shoppingDiv = document.createElement('div')
+    
+    shoppingDiv.classList.add('shopping-div')
+    
+    shoppingDiv.innerHTML = `
+    <li id='check-me'>${newItemValue.newItem}</li>
+    <button class='check-button' onclick='checkItem'>&#10004;</button>
+    <button class='delete-button' onclick='deleteItem(event)'>X</button>
+    `
+    //appen child
+    const ulList = document.getElementById('ul-list')
+    ulList.appendChild(shoppingDiv)
+
+    //empty the form
+    addItemInput.value = ''
+
+
+
+
 })
 
 // deve creare un div contentente:
 // deve creare un 'p' contenente il value dell'input
 // deve avere un tasto delete
 // deve avere un tasto complete che sbarra il testo del 'p' 
+
+
+// make the buttons usable
+
+// check button
+
+const checkItem = function(e) {
+
+    const pressedButton = e.target
+
+    const itemToCheck = pressedButton.getElementById('check-me')
+    console.log(pressedButton)
+
+    itemToCheck.style.textDecoration = 'overline'
+
+}
+
+//delete button
+
+const deleteItem = function(e) {
+    console.log('elimina', e.target.parentElement)
+
+    const pressedButton = e.target
+    const itemToRemove = pressedButton.parentElement
+    itemToRemove.remove()
+}
