454e194cf242519e21e6e81a04b7c035d71481c0
Vincenza <vinni2690@hotmail.com>
2025-02-19 16:22:01 +0100

    html and css complete

---
+++ Shopping list/lista.js
@@ -0,0 +1,14 @@
+
+const pageForm = document.getElementById('new-list-form')
+
+pageForm.addEventListener('submit', (e) => {
+
+    e.preventDefault()
+
+    const addItemInput = document.getElementById('add-item')
+})
+
+// deve creare un div contentente:
+// deve creare un 'p' contenente il value dell'input
+// deve avere un tasto delete
+// deve avere un tasto complete che sbarra il testo del 'p' 
